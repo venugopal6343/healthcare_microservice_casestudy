4e795d81ab09613b78fcc9b8558b9d7bafbd1b78
Ashish.s.panicker <ashish.s.panicker@outlook.com>
2024-08-08 11:01:15 +0530

    Created Patient entity class and enabled JPA Auditing.

---
+++ patient-service/src/main/java/com/healthcare/patient_service/domain/Patient.java
@@ -0,0 +1,48 @@
+package com.healthcare.patient_service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "patients",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "phone")
+        },
+        indexes = {
+                @Index(columnList = "email"),
+                @Index(columnList = "phone")
+        })
+@EntityListeners(AuditingEntityListener.class)
+public class Patient {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String address;
+    private LocalDate dob;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
