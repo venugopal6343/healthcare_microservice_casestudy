f82f6a0d78d484530111bee0ca2cc740b01d9e94
Ashish.s.panicker <ashish.s.panicker@outlook.com>
2024-08-12 14:45:13 +0530

    ValueInListValidator created

--- doctor-service/src/main/java/com/healthcare/doctor_service/dto/DoctorDto.java
+++ doctor-service/src/main/java/com/healthcare/doctor_service/dto/DoctorDto.java
@@ -1,15 +1,32 @@
 package com.healthcare.doctor_service.dto;
 
 import com.healthcare.doctor_service.domain.Doctor;
+import com.healthcare.doctor_service.validators.ValueInList;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
 public record DoctorDto(
         String id,
+
+        @NotBlank(message = "Full name is required")
         String fullName,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email is invalid")
         String email,
+
+        @NotBlank(message = "Phone is required")
+        @Pattern(regexp = "^[0-9]{3}-[0-9]{3}-[0-9]{4}$", message = "Phone is invalid")
         String phone,
+
+        @NotBlank(message = "Address is required")
         String address,
+
+        @ValueInList(allowedValues = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
+                message = "Invalid operation day")
         String[] opDays,
         String[] specialization
 ) {
