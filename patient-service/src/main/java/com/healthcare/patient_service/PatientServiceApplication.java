f1dcccc5ec99503bc775477f537d666b5ec940b5
venugopal6343 <208r1a05a8@gmail.com>
2024-08-27 14:31:20 +0530

    committed eureka

--- patient-service/src/main/java/com/healthcare/patient_service/PatientServiceApplication.java
+++ patient-service/src/main/java/com/healthcare/patient_service/PatientServiceApplication.java
@@ -2,9 +2,11 @@ package com.healthcare.patient_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
